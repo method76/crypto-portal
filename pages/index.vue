2adff95b68bad717c51a542142a0405f30776bf2
Ted Kim <methodtweak@naver.com>
2019-02-01 17:33:47 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -11,85 +11,81 @@ export default {
   .tile.is-vertical>.tile.is-child:not(:last-child) { margin-bottom: 0!important; }
   .panel-icon { width: 80px;height: 80px; }
   .panel-block { font-size: 15px; }
+  .media .tile.is-3 { height: 100px;border: 1px solid #f0f0f0; }
 </style>
 
 <template>
-  <div class="is-fullheight"> <!--  -->
+    <div class="is-fullheight">
     <nav class="level">
-      <div class="block-category-list">
-        <a href="http://portus.orange-themes.net/category/mobile-phones/" class="item"><i class="ti ti-mobile"></i><strong>Mobile Phones</strong></a>
-        <a href="http://portus.orange-themes.net/category/tablets/" class="item"><i class="ti ti-tablet"></i><strong>Tablets</strong></a>
-        <a href="http://portus.orange-themes.net/category/desktops/" class="item"><i class="ti ti-desktop"></i><strong>Desktops</strong></a>
-        <a href="http://portus.orange-themes.net/category/audio/" class="item"><i class="ti ti-headphone"></i><strong>Audio</strong></a>
-        <a href="http://portus.orange-themes.net/tag/gaming/" class="item"><i class="ti ti-game"></i><strong>Gaming</strong></a>
-        <a href="http://portus.orange-themes.net/tag/photo-video/" class="item"><i class="ti ti-camera"></i><strong>Photo &amp; Video</strong></a>
-        <a href="http://portus.orange-themes.net/tag/car-tech/" class="item"><i class="ti ti-car"></i><strong>Car tech</strong></a>
-        <a href="http://portus.orange-themes.net/tag/navigation/" class="item"><i class="ti ti-location-pin"></i><strong>Navigation</strong></a>
-        <a href="http://portus.orange-themes.net/tag/peripherals/" class="item"><i class="ti ti-microphone"></i><strong>Peripherals</strong></a>
-        <a href="http://portus.orange-themes.net/tag/smartwatches/" class="item"><i class="ti ti-time"></i><strong>Smartwatches</strong></a>
-        <a href="http://portus.orange-themes.net/tag/recording/" class="item"><i class="ti ti-mouse"></i><strong>Recording</strong></a>
+      <div class="field">
+        <label class="label">Search Transaction</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="Please input transaction hash">
+        </div>
+      </div>
+    </nav>
+    <nav class="level columns">
+      <div class="column is-3 media">
+        <div class="tile is-ancestor">
+          <div class="tile is-3">Token Post</div>
+          <div class="tile is-3">Hash Net</div>
+          <div class="tile is-3">Block Media</div>
+          <div class="tile is-3">Block Daily</div>
+        </div>
+        <div class="tile is-ancestor">
+          <div class="tile is-3">Coindesk Korea</div>
+          <div class="tile is-3">Investing.com</div>
+          <div class="tile is-3">Decenter</div>
+          <div class="tile is-3">Bit Web</div>
+        </div>
+        <div class="tile is-ancestor">
+          <div class="tile is-3">Block Inpress</div>
+          <div class="tile is-3">Crypto Graph</div>
+          <div class="tile is-3">Blockchain Valley</div>
+          <div class="tile is-3">Blocktimes TV</div>
+        </div>
+        <div class="tile is-ancestor">
+        </div>
+      </div>
+      <div class="column is-6 tile is-ancestor">
+        <div class="tile is-parent">
+          <div class="tile is-child box">
+            <p class="title"><img src="http://portus.orange-themes.net/wp-content/uploads/2016/01/32002310_ml-1200x780_c.jpg"></p>
+          </div>
+        </div>
+        <div class="tile is-3 is-vertical is-parent">
+          <div class="tile is-child box">
+            <p class="title"><img src="http://portus.orange-themes.net/wp-content/uploads/2016/01/street-car-power-fast-600x390_c.jpg"></p>
+          </div>
+          <div class="tile is-child box">
+            <p class="title"><img src="http://portus.orange-themes.net/wp-content/uploads/2016/01/technology-music-sound-things-600x390_c.jpg"></p>
+          </div>
+        </div>
+      </div>
+      <div class="column is-3">
+        <nav class="panel">
+          <p class="panel-heading">Popular Posts</p>
+          <a class="panel-block">
+                <span class="panel-icon">
+                  <img width="80" height="80" src="http://portus.orange-themes.net/wp-content/uploads/2016/01/street-car-power-fast-80x80_c.jpg" alt="Dynamic Prime Factors to make BSB debut" srcset="http://portus.orange-themes.net/wp-content/uploads/2016/01/street-car-power-fast-160x160_c.jpg" data-ot-retina="http://portus.orange-themes.net/wp-content/uploads/2016/01/street-car-power-fast-160x160_c.jpg">
+                </span>
+            <div>Dynamic Prime Factors to make BSB debut</div>
+          </a>
+        </nav>
       </div>
     </nav>
     <nav class="level">
       <div class="columns">
         <div class="column is-12">
-          <div class="tile is-ancestor">
-            <div class="tile is-parent">
-              <div class="tile is-child box">
-                <p class="title"><img src="http://portus.orange-themes.net/wp-content/uploads/2016/01/32002310_ml-1200x780_c.jpg"></p>
-              </div>
+          <div class="widget">
+            <div class="do-space do-space-bg">
+              <a href="http://www.orange-themes.net" target="_blank"><img width="728" height="90" src="http://portus.orange-themes.net/wp-content/themes/portus-premium-theme/images/pob-1.jpg" alt="" title=""></a>
+              <strong>Sponsored advertisement</strong>
             </div>
-            <div class="tile is-3 is-vertical is-parent">
-              <div class="tile is-child box">
-                <p class="title"><img src="http://portus.orange-themes.net/wp-content/uploads/2016/01/street-car-power-fast-600x390_c.jpg"></p>
-              </div>
-              <div class="tile is-child box">
-                <p class="title"><img src="http://portus.orange-themes.net/wp-content/uploads/2016/01/technology-music-sound-things-600x390_c.jpg"></p>
-              </div>
-            </div>
-            <div class="tile is-parent is-3">
-              <nav class="panel">
-                <p class="panel-heading">
-                  Popular Posts__
-                </p>
-                <a class="panel-block">
-                  <span class="panel-icon">
-                    <img width="80" height="80" src="http://portus.orange-themes.net/wp-content/uploads/2016/01/street-car-power-fast-80x80_c.jpg" alt="Dynamic Prime Factors to make BSB debut" srcset="http://portus.orange-themes.net/wp-content/uploads/2016/01/street-car-power-fast-160x160_c.jpg" data-ot-retina="http://portus.orange-themes.net/wp-content/uploads/2016/01/street-car-power-fast-160x160_c.jpg">
-                  </span>
-                  <div>Dynamic Prime Factors to make BSB debut</div>
-                </a>
-              </nav>
-            </div>
+            <!-- END .widget -->
           </div>
         </div>
       </div>
     </nav>
-    <nav class="level">
-      Media
-    </nav>
-    <nav class="level">
-      <div class="columns">
-        <div class="column">Token Post</div>
-        <div class="column">Hash Net</div>
-        <div class="column">Block Media</div>
-        <div class="column">Block Daily</div>
-        <div class="column">Coindesk Korea</div>
-        <div class="column">Investing.com</div>
-        <div class="column">Decenter</div>
-        <div class="column">Bit Web</div>
-        <div class="column">Block Inpress</div>
-        <div class="column">Crypto Graph</div>
-      </div>
-    </nav>
-    <div class="columns">
-      <div class="column is-12">
-        <div class="widget">
-          <div class="do-space do-space-bg">
-            <a href="http://www.orange-themes.net" target="_blank"><img width="728" height="90" src="http://portus.orange-themes.net/wp-content/themes/portus-premium-theme/images/pob-1.jpg" alt="" title=""></a>											<strong>Sponsored advertisement</strong>
-          </div>
-          <!-- END .widget -->
-        </div>
-      </div>
-    </div>
   </div>
 </template>
